98c32d612c62fe130770b94818b127403a1ac208
cxchan1 <cxchan@ualberta.ca>
2021-06-02 03:31:06 -0600

    Simple graphql demo

--- src/typeDefs/index.js
+++
@@ -1,8 +0,0 @@
-  
-const path = require('path');
-const { gql } = require('apollo-server');
-const { importSchema } = require('graphql-import');
-
-const schema = importSchema(path.resolve(__dirname, './schema.graphql'));
-
-module.exports = gql(schema);
